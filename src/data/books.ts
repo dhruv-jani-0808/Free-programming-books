3f7e3cf9f403150499d73e36ac7a4d3f16b332de
cosmicaldrift <shdprogramming@gmail.com>
2025-10-06 20:25:37 +0530

    Updated to include competitive programming category

--- src/data/books.ts
+++ src/data/books.ts
@@ -587,7 +587,8 @@ export const categories = [
   "Cloud Computing",
   "Cyber Security",
   "Computer Networking",
-  "Interview Preparation"
+  "Interview Preparation",
+  "Competitive Programming"
 ];
 
 export const languages = [
